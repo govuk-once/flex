a6d793e00b228ba7129f86d9279dab0da3c22490
Nathan Berry <54477304+nathanberry97@users.noreply.github.com>
2025-11-28 16:08:32 +0000

    Feat: Add functionality to linting pipeline & switch to pnpm (#9)

--- modules/example/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { handler as exampleHandler } from './app/example';
-
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-export const handler = async (event: any) => {
-  console.log('Event received:', event);
-  return await exampleHandler(event);
-};
