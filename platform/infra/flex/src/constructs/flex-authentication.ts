cb71df683bed38fa9515e7c348c4931115393fae
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2026-01-15 09:46:41 +0000

    fix: explicitly install dependencies

--- platform/infra/flex/src/constructs/flex-authentication.ts
+++ platform/infra/flex/src/constructs/flex-authentication.ts
@@ -138,6 +138,9 @@ export class FlexAuthentication extends Construct {
         vpcSubnets,
         // external packages are hoisted to the root of the workspace
         depsLockFilePath: path.join(findRoot(), "pnpm-lock.yaml"),
+        bundling: {
+          nodeModules: ["@aws-lambda-powertools/parameters", "ioredis"],
+        },
       },
     );
 
