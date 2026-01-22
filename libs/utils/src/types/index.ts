db7b937cc9987d567d8186c42e724a1da9b5ad3c
your_github_username <liam.jones@digital.cabinet-office.gov.uk>
2026-01-22 22:52:16 +0000

    initial

--- libs/utils/src/types/index.ts
+++ libs/utils/src/types/index.ts
@@ -1,3 +1,6 @@
+/**
+ * Make all properties in an object type optional, recursively.
+ */
 export type DeepPartial<T> = T extends (...args: unknown[]) => unknown
   ? T
   : T extends Array<infer U>
@@ -5,3 +8,30 @@ export type DeepPartial<T> = T extends (...args: unknown[]) => unknown
     : T extends object
       ? { [K in keyof T]?: DeepPartial<T[K]> }
       : T;
+
+/**
+ * Remove a specific suffix from a string type.
+ */
+export type WithoutSuffix<
+  T,
+  SUFFIX extends string,
+> = T extends `${infer P}${SUFFIX}` ? P : T;
+
+/**
+ * Remove a specific suffix from all keys in an object type recursively.
+ */
+export type WithoutPropSuffix<T, SUFFIX extends string> = {
+  [K in keyof T as WithoutSuffix<K, SUFFIX>]: T[K] extends object
+    ? WithoutPropSuffix<T[K], SUFFIX>
+    : T[K];
+};
+
+/**
+ * Simplify a type by resolving intersections and flattening mapped types.
+ * This is useful for improving readability of complex types in IDEs.
+ */
+export type Simplify<T> = T extends object
+  ? {
+      [P in keyof T]: Simplify<T[P]>;
+    }
+  : T;
