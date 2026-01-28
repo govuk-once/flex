df34c556d03a1cc97d6d71671bcba77bbaa66d41
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2026-01-28 07:49:14 +0000

    feat: allow secrets manager middleware options

--- libs/middlewares/src/secrets/index.ts
+++ libs/middlewares/src/secrets/index.ts
@@ -1,7 +1,8 @@
-// libs/middlewares/src/secrets.ts
 import { MiddlewareObj } from "@middy/core";
 import secretsManager, { secret } from "@middy/secrets-manager";
 import type { Context } from "aws-lambda";
+
+type SecretsManagerOptions = Parameters<typeof secretsManager>[0];
 
 export function createSecretsMiddleware<
   TSecrets extends Record<string, string | undefined> = Record<
@@ -13,8 +14,14 @@ export function createSecretsMiddleware<
    * Maps context property name -> Secrets Manager secret id
    */
   secrets: { [K in keyof TSecrets & string]: string | undefined };
+  /**
+   * Additional options to pass through to secretsManager.
+   * These options will be merged with the default configuration.
+   */
+  options?: Omit<SecretsManagerOptions, "fetchData" | "setToContext">;
 }): MiddlewareObj<unknown, unknown, Error, Context & TSecrets> {
   return secretsManager({
+    ...opts.options,
     fetchData: Object.fromEntries(
       Object.entries(opts.secrets).map(([key, secretId]) => {
         if (!secretId) {
