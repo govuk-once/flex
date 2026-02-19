c2d6c779095df9ca32f3f3a8f8c5ef2d0b82e33f
Thomas Drake <tom@tom-drake.co.uk>
2026-02-19 15:52:46 +0000

    fix: Fail when this step fails

--- platform/infra/core/src/app.ts
+++ platform/infra/core/src/app.ts
@@ -6,9 +6,10 @@ import { FlexCoreStack } from "./stack";
 const { persistent } = getEnvConfig();
 
 if (!persistent) {
-  throw new Error(
+  console.log(
     "Skipping deployment: This is a persistent stack and will only be deployed when STAGE is set to an approved stack",
   );
+  process.exit(0);
 }
 
 const app = new cdk.App();
