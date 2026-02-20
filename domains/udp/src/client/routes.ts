d4d4ed6dfa5688d5c4fb04efb7c79c46e980d3b2
Paz Saragi <76964496+pazsaragi@users.noreply.github.com>
2026-02-20 14:55:26 +0000

    feat: udp internal domain client (#117)
    * feat: add udp domain to internal gateway client as part of ACL implementation

---
+++ domains/udp/src/client/routes.ts
@@ -0,0 +1,11 @@
+export const UDP_GATEWAY_BASE = "/gateways/udp/v1";
+
+export const UDP_GATEWAY_ROUTES = {
+  preferences: "/preferences",
+} as const;
+
+export const UDP_DOMAIN_BASE = "/domains/udp/v1";
+
+export const UDP_DOMAIN_ROUTES = {
+  createUser: "/user",
+} as const;
---
+++ domains/udp/src/schemas/preferences.ts
@@ -0,0 +1,23 @@
+import { IsoDateTime } from "@flex/utils";
+import { z } from "zod";
+
+export const CONSENT_STATUS_SCHEMA = z.enum(["unknown", "accepted", "denied"]);
+
+export const preferencesRequestSchema = z.object({
+  notifications: z.object({
+    consentStatus: CONSENT_STATUS_SCHEMA,
+  }),
+});
+
+export type PreferencesRequest = z.infer<typeof preferencesRequestSchema>;
+
+export const preferencesResponseSchema = z.object({
+  preferences: z.object({
+    notifications: z.object({
+      consentStatus: CONSENT_STATUS_SCHEMA,
+      updatedAt: IsoDateTime,
+    }),
+  }),
+});
+
+export type PreferencesResponse = z.infer<typeof preferencesResponseSchema>;
