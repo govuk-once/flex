1828a4771b38da0d346e07040d35e7911e7f64f7
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2026-03-02 10:18:02 +0000

    fix: remove internal user get endpoint

--- domains/udp/domain.private.config.ts
+++ domains/udp/domain.private.config.ts
@@ -6,22 +6,6 @@ export const endpoints = defineDomain({
     v1: {
       routes: {
         "/users": {
-          GET: {
-            type: "ISOLATED",
-            entry: "handlers/internal/v1/user/get.ts",
-            envEphemeral: {
-              FLEX_PRIVATE_GATEWAY_URL_PARAM_NAME:
-                "/flex-core/private-gateway/url",
-            },
-            timeoutSeconds: 20,
-            permissions: [
-              {
-                type: "gateway",
-                path: "notifications",
-                method: "POST",
-              },
-            ],
-          },
           POST: {
             type: "ISOLATED",
             entry: "handlers/internal/v1/user/post.ts",
