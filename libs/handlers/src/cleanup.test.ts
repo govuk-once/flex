d7bfbd87d5f1a28aa2e208542efbc1c42c023fe0
your_github_username <liam.jones@digital.cabinet-office.gov.uk>
2026-03-04 08:49:09 +0000

    additional test to cover no deleting path

--- libs/handlers/src/cleanup.test.ts
+++ libs/handlers/src/cleanup.test.ts
@@ -1,13 +1,18 @@
 import { it } from "@flex/testing";
 import fs, { readdirSync, rmSync } from "fs";
 import { vol } from "memfs";
-import { describe, expect, vi, vitest } from "vitest";
+import { beforeEach, describe, expect, vi, vitest } from "vitest";
 
 import { clearTmp } from "./cleanup";
 
 vitest.mock("node:fs", async () => {
   const fs = await import("memfs");
   return fs;
+});
+
+beforeEach(() => {
+  vol.reset();
+  vi.clearAllMocks();
 });
 
 describe("clearTmp", () => {
@@ -33,5 +38,22 @@ describe("clearTmp", () => {
       recursive: true,
       force: true,
     });
+  });
+
+  it("should not clear files in /tmp when NODE_ENV is test", ({ env }) => {
+    env.set({ NODE_ENV: "test" });
+
+    vol.fromJSON({
+      "/tmp/file1.txt": "content1",
+      "/tmp/file2.txt": "content2",
+    });
+
+    vi.spyOn(fs, "readdirSync");
+    vi.spyOn(fs, "rmSync");
+
+    clearTmp();
+
+    expect(readdirSync).not.toHaveBeenCalled();
+    expect(rmSync).not.toHaveBeenCalled();
   });
 });
