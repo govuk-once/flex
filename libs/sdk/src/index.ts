ec644727c856a133eb4ed774cb6c90525f472888
your_github_username <liam.jones@digital.cabinet-office.gov.uk>
2026-03-24 21:19:01 +0000

    fixed some type locations

--- libs/sdk/src/index.ts
+++ libs/sdk/src/index.ts
@@ -2,26 +2,4 @@ export * from "./config";
 export type { IDomain, IDomainEndpoint, Permission } from "./domain";
 export { defineDomain, domain, domainSchema } from "./domain";
 export * from "./route";
-export type {
-  DomainConfig,
-  DomainIntegration,
-  DomainIntegrationOptions,
-  DomainResource,
-  DomainResult,
-  DomainRoutes,
-  FlexLambdaHandler,
-  FunctionConfig,
-  HandlerResult,
-  HeaderConfig,
-  HttpMethod,
-  InferDomainIntegrationOptions,
-  InferRouteContext,
-  IntegrationDomainService,
-  IntegrationResult,
-  IntegrationServiceGateway,
-  LambdaAuthorizerContext,
-  LogLevel,
-  RouteAccess,
-  RouteAuth,
-  RouteHandler,
-} from "./types";
+export type * from "./types";
