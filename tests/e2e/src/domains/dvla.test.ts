6c55216cc61a6d9c0066d5d23a9513f7ec3aa08d
Nathan Berry <54477304+nathanberry97@users.noreply.github.com>
2026-03-31 09:01:43 +0100

    chore: update e2e tests for udp linking and dvla domain (#197)

--- tests/e2e/src/domains/dvla.test.ts
+++ tests/e2e/src/domains/dvla.test.ts
@@ -1,7 +1,8 @@
 import { SSMProvider } from "@aws-lambda-powertools/parameters/ssm";
 import { viewDriverResponseSchema } from "@flex/dvla-domain";
-import { it } from "@flex/testing/e2e";
-import { describe, expect, inject } from "vitest";
+import { beforeAll, describe, expect, inject } from "vitest";
+
+import { it } from "../extend/it";
 
 describe("DVLA domain", () => {
   const { JWT, ENVIRONMENT } = inject("e2eEnv");
@@ -11,34 +12,24 @@ describe("DVLA domain", () => {
 
   describe("/dvla/v1/driving-licence", () => {
     const endpoint = "/dvla/v1/driving-licence";
+    let linkingId: string;
+
+    beforeAll(async () => {
+      const rawLinkingId = await ssmProvider.get<string>(
+        `/${ENVIRONMENT}/flex-param/dvla/test-user`,
+      );
+      if (!rawLinkingId)
+        throw new Error(`Parameter not found for environment: ${ENVIRONMENT}`);
+
+      linkingId = rawLinkingId;
+    });
 
     describe("GET", () => {
-      it("rejects unauthenticated requests", async ({ cloudfront }) => {
-        const result = await cloudfront.client.get(endpoint);
-
-        expect(result.status).toBe(401);
-
-        expect(result.headers.get("x-rejected-by")).toBe("cloudfront-function");
-      });
-
       it("returns 200 and valid data when identity is linked", async ({
         cloudfront,
+        withIdentityLink,
       }) => {
-        const linkingId = await ssmProvider.get<string>(
-          `/${ENVIRONMENT}/flex-param/dvla/test-user`,
-        );
-
-        if (!linkingId) {
-          throw new Error(
-            `Parameter not found for environment: ${ENVIRONMENT}`,
-          );
-        }
-
-        const postLinkingId = `/udp/v1/identity/dvla/${linkingId}`;
-        const setupResult = await cloudfront.client.post(postLinkingId, {
-          headers: { ...authorization },
-        });
-        expect([201, 204]).toContain(setupResult.status);
+        await withIdentityLink("dvla", linkingId);
 
         const result = await cloudfront.client.get(endpoint, {
           headers: { ...authorization },
@@ -47,6 +38,18 @@ describe("DVLA domain", () => {
 
         const validation = viewDriverResponseSchema.safeParse(result.body);
         expect(validation.success).toBe(true);
+      });
+
+      it("returns 404 when user is not linked", async ({
+        cloudfront,
+        withCleanIdentity,
+      }) => {
+        await withCleanIdentity("dvla");
+
+        const result = await cloudfront.client.get(endpoint, {
+          headers: { ...authorization },
+        });
+        expect(result.status).toBe(404);
       });
     });
   });
