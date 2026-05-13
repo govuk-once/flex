3c5c183cc678ef8c5562935e93efd071aae18b5c
David Malheiro <davidsmalheiro@gmail.com>
2026-05-13 17:27:53 +0100

    fix: zap debug env file write

--- scripts/dastSetup.ts
+++ scripts/dastSetup.ts
@@ -16,6 +16,8 @@ async function main() {
     console.log("::add-mask::token");
 
     const envFile = process.env.GITHUB_ENV;
+    console.log(`\nwritting in: ${envFile}\n`);
+
     if (envFile) {
       // Append env var in github env file to share with the next step
       fs.appendFileSync(envFile, `ZAP_AUTH_HEADER_VALUE=Bearer ${token}\n`);
