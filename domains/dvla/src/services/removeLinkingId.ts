11c43f99f986281d66b52aa97abd13c0f5a8a10a
Nathan Berry <nathan.berry@digital.cabinet-office.gov.uk>
2026-05-21 16:23:02 +0100

    chore: fix linting issue

--- domains/dvla/src/services/removeLinkingId.ts
+++ domains/dvla/src/services/removeLinkingId.ts
@@ -3,8 +3,8 @@
  */
 export const removeLinkingId = <T extends Record<string, unknown>>(
   data: T | null | undefined,
-): Omit<T, "linkingId"> | null | undefined => {
-  if (!data) return data;
+): Omit<T, "linkingId"> | undefined => {
+  if (!data) return undefined;
 
   const { linkingId: _linkingId, ...body } = data;
   return body;
