62211244008e0fe236bac03ae750f072a0ad57b3
Paz Saragi <parulian.saragi@digital.cabinet-office.gov.uk>
2026-05-22 09:20:17 +0100

    chore: test an untested function fails coverage

---
+++ platform/domains/dvla/src/utils/untest.ts
@@ -0,0 +1,4 @@
+export const someUntestedFunction = () => {
+  // This function is intentionally left untested for demonstration purposes.
+  console.log("This function is not covered by tests.");
+};
