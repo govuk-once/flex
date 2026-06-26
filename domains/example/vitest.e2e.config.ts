ee7093d5dc6c9dedd1784814a6d42fcabd0ee7b1
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2026-06-26 10:23:42 +0100

    fix(NOT-311): Updating test timeout in the example domain

--- domains/example/vitest.e2e.config.ts
+++ domains/example/vitest.e2e.config.ts
@@ -1,1 +1,12 @@
-export { e2eConfig as default } from "@flex/config/vitest/e2e";
+import { e2eConfig } from "@flex/config/vitest/e2e";
+import { defineConfig, mergeConfig } from "vitest/config";
+
+// UNS & UDP Tests have longer timeouts in their own domains - this should match that duration
+export default mergeConfig(
+  e2eConfig,
+  defineConfig({
+    test: {
+      testTimeout: 40_000,
+    },
+  }),
+);
