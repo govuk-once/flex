576b7f0c8a330329a31c8af8b2f7eeb64108eab8
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2026-06-26 11:03:33 +0100

    fix(NOT-311): investigating failing E2E tests for UNS (#382)
    * fix(NOT-311): Matching test timeouts in UNS to UDP
    
    * fix(NOT-311): Schema update
    
    * fix(NOT-311): Updating tests in example domain
    
    * fix(NOT-311): Updating test timeout in the example domain

--- domains/uns/vitest.e2e.config.ts
+++ domains/uns/vitest.e2e.config.ts
@@ -1,1 +1,12 @@
-export { e2eConfig as default } from "@flex/config/vitest/e2e";
+import { e2eConfig } from "@flex/config/vitest/e2e";
+import { defineConfig, mergeConfig } from "vitest/config";
+
+// UNS's E2E calls are slow on lower environments due to cold starts
+export default mergeConfig(
+  e2eConfig,
+  defineConfig({
+    test: {
+      testTimeout: 40_000,
+    },
+  }),
+);
