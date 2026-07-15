05e4b6f05ad1e7a6c04f3cc09c53d95267f70464
Nathan Berry <54477304+nathanberry97@users.noreply.github.com>
2026-07-15 13:10:22 +0100

    GOVUKAPP-3762 chore: forward error codes to front end for licence 404 errors (#414)
    * chore: update error format to match what DVLA send to us
    
    * chore: resolve the comments on PR

--- domains/dvla/src/handlers/v1/customer/licence/get.ts
+++ domains/dvla/src/handlers/v1/customer/licence/get.ts
@@ -1,6 +1,7 @@
 import { route } from "@domain";
 import { getDvlaAuthToken, getUserLinkingId } from "@services/authentication";
 import { handleStandardErrors } from "@services/errors";
+import { isHttpError } from "http-errors";
 import { status } from "http-status";
 
 const endpoint = "GET /v1/customer/licence";
@@ -8,17 +9,45 @@ const endpoint = "GET /v1/customer/licence";
 export const handler = route(endpoint, async (ctx) => {
   const { integrations } = ctx;
 
-  const [userLinkingId, auth] = await Promise.all([
-    getUserLinkingId(ctx),
-    getDvlaAuthToken(ctx),
-  ]);
+  try {
+    const [userLinkingId, auth] = await Promise.all([
+      getUserLinkingId(ctx),
+      getDvlaAuthToken(ctx),
+    ]);
 
-  const response = await integrations.dvlaGetCustomerLicence({
-    headers: { auth },
-    query: { linkingId: userLinkingId },
-  });
+    const response = await integrations.dvlaGetCustomerLicence({
+      headers: { auth },
+      query: { linkingId: userLinkingId },
+    });
 
-  handleStandardErrors(response, endpoint);
+    handleStandardErrors(response, endpoint);
 
-  return { status: status.OK, data: response.data };
+    return { status: status.OK, data: response.data };
+  } catch (error: unknown) {
+    /**
+     * TARGETED ERROR INTERCEPTION
+     *  - Only intercept if it's a 404 AND has an explicit provider code
+     *    attached. The reason for this is that DVLA may return the following
+     *    two error codes: GUK-404-04, GUK-404-05
+     *    In which the app team will need to be able show different states on
+     *    these 404 errors
+     */
+    if (
+      isHttpError(error) &&
+      error.statusCode === status.NOT_FOUND &&
+      "code" in error &&
+      typeof error.code === "string" &&
+      (error.code === "GUK-404-04" || error.code === "GUK-404-05")
+    ) {
+      return {
+        status: status.NOT_FOUND,
+        error: {
+          code: error.code,
+          message: error.message || "Not Found",
+        },
+      };
+    }
+
+    throw error;
+  }
 });
