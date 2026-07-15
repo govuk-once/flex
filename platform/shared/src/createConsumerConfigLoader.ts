d134e17ea63071aebca5efe29de7a41ab49e4754
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2026-07-15 22:54:12 +0100

    spike(NOT-325): Reverting to original flow using powertools

--- platform/shared/src/createConsumerConfigLoader.ts
+++ platform/shared/src/createConsumerConfigLoader.ts
@@ -9,21 +9,21 @@ export function createConsumerConfigLoader<T>(schema: z.ZodType<T>) {
   return async function loadConsumerConfig(secretArn: string): Promise<T> {
     // Wild attempt at getting move verbose error responses
     console.log(`Fetching config using getSecret`);
-    if (secretArn.includes(`uns-dev`)) {
-      return schema.parseAsync(
-        JSON.parse(
-          (
-            await new SecretsManagerClient({
-              region: "eu-west-2",
-            }).send(
-              new GetSecretValueCommand({
-                SecretId: secretArn,
-              }),
-            )
-          ).SecretString ?? "{}",
-        ),
-      );
-    }
+    // // // if (secretArn.includes(`uns-dev`)) {
+    // // //   return schema.parseAsync(
+    // // //     JSON.parse(
+    // // //       (
+    // // //         await new SecretsManagerClient({
+    // // //           region: "eu-west-2",
+    // // //         }).send(
+    // // //           new GetSecretValueCommand({
+    // // //             SecretId: secretArn,
+    // // //           }),
+    // // //         )
+    // // //       ).SecretString ?? "{}",
+    // // //     ),
+    // // //   );
+    // // // }
     const config = await getSecret<T>(secretArn, {
       transform: "json",
       maxAge: 600,
