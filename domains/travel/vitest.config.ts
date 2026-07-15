44fed622f32bf59c3eb7a7643948cc30b55fc8de
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-07-15 06:34:18 +0100

    chore: pre-commit

--- domains/travel/vitest.config.ts
+++ domains/travel/vitest.config.ts
@@ -9,7 +9,7 @@ export default mergeConfig(
       env: {
         AWS_REGION: "eu-west-2",
         AWS_ACCESS_KEY_ID: "test",
-        AWS_SECRET_ACCESS_KEY: "test",
+        AWS_SECRET_ACCESS_KEY: "test", // pragma: allowlist secret`
         privateGatewayUrl: "https://execute-api.eu-west-2.amazonaws.com",
       },
     },
