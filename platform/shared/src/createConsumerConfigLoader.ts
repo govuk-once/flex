dd18652d24b2eff5709f8b56950155275e39b270
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2026-07-15 21:00:51 +0100

    spike(NOT-325): Investigating another method of getting more detailed errors

--- platform/shared/src/createConsumerConfigLoader.ts
+++ platform/shared/src/createConsumerConfigLoader.ts
@@ -1,12 +1,33 @@
 import { getSecret } from "@aws-lambda-powertools/parameters/secrets";
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
 import { z } from "zod";
 
 export function createConsumerConfigLoader<T>(schema: z.ZodType<T>) {
   return async function loadConsumerConfig(secretArn: string): Promise<T> {
+    // Wild attempt at getting move verbose error responses
     console.log(`Fetching config using getSecret`);
+    if (secretArn.includes(`uns-dev`)) {
+      return schema.parseAsync(
+        JSON.parse(
+          (
+            await new SecretsManagerClient({
+              region: "eu-west-2",
+            }).send(
+              new GetSecretValueCommand({
+                SecretId: secretArn,
+              }),
+            )
+          ).SecretString ?? "{}",
+        ),
+      );
+    }
     const config = await getSecret<T>(secretArn, {
       transform: "json",
       maxAge: 600,
+      forceFetch: true,
     });
 
     if (!config) {
