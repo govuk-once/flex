879bbed1ba5e983272cc2a56bd9dd3c65018daac
Thomas Drake <tom@tom-drake.co.uk>
2026-07-20 13:59:09 +0100

    fix: Add correlation id search to powertools logger

--- libs/logging/src/logger.ts
+++ libs/logging/src/logger.ts
@@ -1,16 +1,24 @@
 import { Logger as PowerToolsLogger } from "@aws-lambda-powertools/logger";
+import { search } from "@aws-lambda-powertools/logger/correlationId";
 import { LogLevel } from "@aws-lambda-powertools/logger/types";
 
 import { FlexLogFormatter } from "./formatter";
 
+type FlexLoggerOptions = Omit<
+  ConstructorParameters<typeof PowerToolsLogger>[0],
+  "logFormatter" | "logRecordOrder"
+>;
+
 class FlexLogger extends PowerToolsLogger {
   readonly #formatter: FlexLogFormatter;
 
-  constructor() {
+  constructor(options: FlexLoggerOptions = {}) {
     const formatter = new FlexLogFormatter();
     super({
       logLevel: "INFO",
+      ...options,
       logFormatter: formatter,
+      correlationIdSearchFn: search,
     });
 
     this.#formatter = formatter;
