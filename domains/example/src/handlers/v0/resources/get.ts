807d566722c45abeffaee6bd636fff08ab42ae29
Thomas Drake <tom@tom-drake.co.uk>
2026-07-28 13:21:14 +0100

    chore: Update packages and actions

--- domains/example/src/handlers/v0/resources/get.ts
+++ domains/example/src/handlers/v0/resources/get.ts
@@ -1,23 +1,20 @@
 import { route } from "@domain";
 
-export const handler = route(
-  "GET /v0/resources",
-  async ({ logger, resources }) => {
-    const {
-      privateGatewayUrl,
-      encryptionKeyArn,
-      udpNotificationSecret, // pragma: allowlist secret
-    } = resources;
+export const handler = route("GET /v0/resources", ({ logger, resources }) => {
+  const {
+    privateGatewayUrl,
+    encryptionKeyArn,
+    udpNotificationSecret, // pragma: allowlist secret
+  } = resources;
 
-    logger.info("Deploy time resources resolved", resources);
+  logger.info("Deploy time resources resolved", resources);
 
-    return await Promise.resolve({
-      status: 200,
-      data: {
-        ssm: { param: privateGatewayUrl.length },
-        secret: { secret: udpNotificationSecret.length }, // pragma: allowlist secret
-        kms: { key: encryptionKeyArn.length },
-      },
-    });
-  },
-);
+  return {
+    status: 200,
+    data: {
+      ssm: { param: privateGatewayUrl.length },
+      secret: { secret: udpNotificationSecret.length }, // pragma: allowlist secret
+      kms: { key: encryptionKeyArn.length },
+    },
+  };
+});
