da9447dfbc221b99cfb7056cceb0d0bfb09d8d3c
Thomas Drake <tom@tom-drake.co.uk>
2026-07-30 11:23:06 +0100

    FLEX-000 chore: Update packages and actions (#451)
    chore: Update packages and actions

--- domains/example/src/handlers/v0/headers/get.ts
+++ domains/example/src/handlers/v0/headers/get.ts
@@ -1,16 +1,16 @@
 import { route } from "@domain";
 
-export const handler = route("GET /v0/headers", async ({ headers, logger }) => {
+export const handler = route("GET /v0/headers", ({ headers, logger }) => {
   const { requestId, correlationId, exampleId } = headers;
 
   logger.info("Headers received", { requestId, correlationId, exampleId });
 
-  return await Promise.resolve({
+  return {
     status: 200,
     data: {
       requestId,
       correlationId: correlationId ?? null,
       exampleId: exampleId ?? null,
     },
-  });
+  };
 });
