a7839a38592b64b27544d009207b3effb8023d96
Nathan Berry <nathan.berry@digital.cabinet-office.gov.uk>
2026-08-12 14:42:36 +0100

    chore: re-enable dvla e2e tests within flex

--- domains/dvla/e2e/dvla.test.ts
+++ domains/dvla/e2e/dvla.test.ts
@@ -1,81 +1,174 @@
-import { SSMProvider } from "@aws-lambda-powertools/parameters/ssm";
-import { vehicleEnquiryResponseSchema } from "@flex/dvla-service-gateway";
+import {
+  customerDriversLicenceSchema,
+  customerVehicleDetailsSchema,
+  customerVehiclesResponseSchema,
+  SingleShareCodeResponseSchema,
+  vehicleEnquiryResponseSchema,
+} from "@flex/dvla-service-gateway";
 import { isDomainDeployed, isRouteDeployed } from "@flex/sdk";
-import { it } from "@flex/testing/e2e";
-import { config as udpConfig } from "@flex/udp-domain/config";
+import { createApi, it } from "@flex/testing/e2e";
+import { status } from "http-status";
 import { beforeAll, describe, expect, inject } from "vitest";
 
 import { config as dvlaConfig } from "../domain.config";
 
-const ssmProvider = new SSMProvider();
+describe.runIf(isDomainDeployed(dvlaConfig))("DVLA domain", () => {
+  describe("/dvla/v1/customer", () => {
+    const endpointBase = "/dvla/v1/customer";
 
-const udpCreateIdentityDeployed = () =>
-  isRouteDeployed(udpConfig, "POST /v1/identity/:service");
-const udpDeleteIdentityDeployed = () =>
-  isRouteDeployed(udpConfig, "DELETE /v1/identity/:service");
+    describe.runIf(isRouteDeployed(dvlaConfig, "GET /v1/customer/licence"))(
+      "/licence",
+      () => {
+        it("returns 200 with valid licence data", async ({
+          cloudfront,
+          authHeader,
+        }) => {
+          const result = await cloudfront.client.get(
+            `${endpointBase}/licence`,
+            {
+              headers: authHeader,
+            },
+          );
 
-// TODO need to updating linking process to match new POST endpoint
-describe.runIf(isDomainDeployed(dvlaConfig)).todo("DVLA domain", () => {
-  const { ENVIRONMENT } = inject("e2eEnv");
-  let linkingId: string;
+          expect(result.status).toBe(status.OK);
+          const validation = customerDriversLicenceSchema.safeParse(
+            result.body,
+          );
 
-  beforeAll(async () => {
-    const rawLinkingId = await ssmProvider.get<string>(
-      `/${ENVIRONMENT}/flex-param/dvla/test-user`,
+          expect(validation.success).toBe(true);
+        });
+      },
     );
 
-    if (!rawLinkingId) {
-      throw new Error(`Parameter not found for environment: ${ENVIRONMENT}`);
-    }
+    describe.runIf(
+      isRouteDeployed(dvlaConfig, "GET /v1/customer/vehicles") &&
+        isRouteDeployed(dvlaConfig, "GET /v1/customer/vehicle/:id"),
+    )("/vehicles", () => {
+      let vehicleId: string | undefined;
 
-    linkingId = rawLinkingId;
+      beforeAll(async () => {
+        const { FLEX_API_URL, JWT, E2E_BYPASS_TOKEN } = inject("e2eEnv");
+        const client = createApi(`${FLEX_API_URL}/app`);
+
+        const result = await client.client.get(`${endpointBase}/vehicles`, {
+          headers: {
+            Authorization: `Bearer ${JWT.VALID}`,
+            "x-flex-e2e-bypass": E2E_BYPASS_TOKEN,
+          },
+        });
+
+        const validation = customerVehiclesResponseSchema.safeParse(
+          result.body,
+        );
+        if (validation.success) {
+          vehicleId = validation.data.customerVehicles[0]?.vehicleId.toString();
+        }
+      });
+
+      it("returns 200 with valid vehicles data", async ({
+        cloudfront,
+        authHeader,
+      }) => {
+        const result = await cloudfront.client.get(`${endpointBase}/vehicles`, {
+          headers: authHeader,
+        });
+
+        expect(result.status).toBe(status.OK);
+        const validation = customerVehiclesResponseSchema.safeParse(
+          result.body,
+        );
+        expect(validation.success).toBe(true);
+      });
+
+      it("returns 200 with valid vehicle data", async ({
+        cloudfront,
+        authHeader,
+      }) => {
+        expect(vehicleId, "Expected a valid vehicleId").toBeDefined();
+
+        const result = await cloudfront.client.get(
+          `${endpointBase}/vehicle/${vehicleId ?? ""}`,
+          {
+            headers: authHeader,
+          },
+        );
+
+        expect(result.status).toBe(status.OK);
+        const validation = customerVehicleDetailsSchema.safeParse(result.body);
+        expect(validation.success).toBe(true);
+      });
+    });
+  });
+
+  describe("/dvla/share-code", () => {
+    const endpointBase = "/dvla/v1/share-code";
+    let sharecodeId: string | undefined;
+
+    describe.runIf(
+      isRouteDeployed(dvlaConfig, "POST /v1/share-code") &&
+        isRouteDeployed(dvlaConfig, "POST /v1/share-code/:id/cancel"),
+    )("/dvla/v1/share-code & /dvla/v1/share-code/:id/cancel", () => {
+      it("creates and then proceeds to cancel newly created share-code", async ({
+        cloudfront,
+        authHeader,
+      }) => {
+        const result = await cloudfront.client.post(endpointBase, {
+          headers: authHeader,
+          body: {},
+        });
+
+        expect(result.status).toBe(status.OK);
+        const validation = SingleShareCodeResponseSchema.safeParse(result.body);
+
+        expect(validation.success).toBe(true);
+
+        if (validation.success) {
+          sharecodeId = validation.data.tokenId;
+          const result = await cloudfront.client.post(
+            `${endpointBase}/${sharecodeId}/cancel`,
+            {
+              headers: authHeader,
+              body: {},
+            },
+          );
+          expect(result.status).toBe(status.OK);
+          const validationCancel = SingleShareCodeResponseSchema.safeParse(
+            result.body,
+          );
+          expect(validationCancel.success).toBe(true);
+        }
+      });
+    });
   });
 
   describe("/dvla/v1/test-notification", () => {
     const endpoint = "/dvla/v1/test-notification";
 
-    describe.runIf(
-      isRouteDeployed(dvlaConfig, "POST /v1/test-notification") &&
-        udpCreateIdentityDeployed() &&
-        udpDeleteIdentityDeployed(),
-    )("POST", () => {
-      it("returns 202 when identity is linked and notification is sent", async ({
-        cloudfront,
-        withIdentityLink,
-        authHeader,
-      }) => {
-        await withIdentityLink("dvla", linkingId);
+    describe.runIf(isRouteDeployed(dvlaConfig, "POST /v1/test-notification"))(
+      "POST",
+      () => {
+        it("returns 202 when identity is linked and notification is sent", async ({
+          cloudfront,
+          authHeader,
+        }) => {
+          const result = await cloudfront.client.post(endpoint, {
+            headers: authHeader,
+            body: {},
+          });
 
-        const result = await cloudfront.client.post(endpoint, {
-          headers: authHeader,
-          body: {},
+          expect(result.status).toBe(status.ACCEPTED);
         });
-
-        expect(result.status).toBe(202);
-      });
-
-      it("returns 404 when user has no identity link", async ({
-        cloudfront,
-        withCleanIdentity,
-        authHeader,
-      }) => {
-        await withCleanIdentity("dvla");
-
-        const result = await cloudfront.client.post(endpoint, {
-          headers: authHeader,
-          body: {},
-        });
-
-        expect(result.status).toBe(404);
-      });
-    });
+      },
+    );
   });
 
   describe("/dvla/v1/vehicle-enquiry", () => {
     const mockRegistration = {
-      valid: "AA19AAA",
+      badRequest: "ER19BAD",
       notFound: "ER19NFD",
+      tooManyRequests: "ER19THR",
       upstreamError: "ER19ERR",
+      valid: "AA19AAA",
     };
     const endpoint = (registration: string) =>
       `/dvla/v1/vehicle-enquiry/${registration}`;
@@ -92,24 +185,12 @@ describe.runIf(isDomainDeployed(dvlaConfig)).todo("DVLA domain", () => {
             { headers: authHeader },
           );
 
-          expect(result.status).toBe(200);
+          expect(result.status).toBe(status.OK);
 
           const validation = vehicleEnquiryResponseSchema.safeParse(
             result.body,
           );
           expect(validation.success).toBe(true);
-        });
-
-        it("returns 404 for a non-existent vehicle registration", async ({
-          cloudfront,
-          authHeader,
-        }) => {
-          const result = await cloudfront.client.get(
-            endpoint(mockRegistration.notFound),
-            { headers: authHeader },
-          );
-
-          expect(result.status).toBe(404);
         });
 
         it("returns 502 when upstream returns a 500 error", async ({
@@ -121,8 +202,50 @@ describe.runIf(isDomainDeployed(dvlaConfig)).todo("DVLA domain", () => {
             { headers: authHeader },
           );
 
-          expect(result.status).toBe(502);
+          expect(result.status).toBe(status.BAD_GATEWAY);
         });
+
+        /**
+         * The below are no longer working with the new VES service DVLA are
+         * providing us, now returning 500 for these codes
+         * - Ask DVLA to look into their new service, will update the below once
+         *   resolved
+         */
+        it.todo(
+          "returns 404 for a non-existent vehicle registration",
+          async ({ cloudfront, authHeader }) => {
+            const result = await cloudfront.client.get(
+              endpoint(mockRegistration.notFound),
+              { headers: authHeader },
+            );
+
+            expect(result.status).toBe(status.NOT_FOUND);
+          },
+        );
+
+        it.todo(
+          "returns 400 for a bad vehicle registration request",
+          async ({ cloudfront, authHeader }) => {
+            const result = await cloudfront.client.get(
+              endpoint(mockRegistration.badRequest),
+              { headers: authHeader },
+            );
+
+            expect(result.status).toBe(status.BAD_REQUEST);
+          },
+        );
+
+        it.todo(
+          "returns 429 for when too many requests hits their service",
+          async ({ cloudfront, authHeader }) => {
+            const result = await cloudfront.client.get(
+              endpoint(mockRegistration.tooManyRequests),
+              { headers: authHeader },
+            );
+
+            expect(result.status).toBe(status.TOO_MANY_REQUESTS);
+          },
+        );
       },
     );
   });
