db2577e706e005f1c16f13fb753b4a7e8780572c
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-08-17 19:41:52 +0100

    fix: pragma allowlist

--- platform/domains/travel/vitest.config.ts
+++ platform/domains/travel/vitest.config.ts
@@ -1,5 +1,5 @@
 import { config } from "@flex/config/vitest";
-import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
 
 export default mergeConfig(
   config,
@@ -9,8 +9,8 @@ export default mergeConfig(
       setupFiles: ["@flex/testing/setup/service-gateway"],
       env: {
         AWS_REGION: "eu-west-2",
-        AWS_ACCESS_KEY_ID: "test-access-id",
-        AWS_SECRET_ACCESS_KEY: "test-secret-access-key",
+        AWS_ACCESS_KEY_ID: "test-access-id", // pragma: allowlist secret
+        AWS_SECRET_ACCESS_KEY: "test-secret-access-key", // pragma: allowlist secret
       },
     },
   }),
