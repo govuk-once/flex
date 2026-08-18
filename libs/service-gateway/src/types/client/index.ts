22210f86826e58ddcc5c262a9630748b3e4510ea
Richard Ellis <richard.ellis@digital.cabinet-office.gov.uk>
2026-08-18 09:03:29 +0100

    fix: lint

--- libs/service-gateway/src/types/client/index.ts
+++ libs/service-gateway/src/types/client/index.ts
@@ -4,9 +4,9 @@ import type { DynamoClient } from "./dynamo";
 import type { EventBusClient } from "./event-bus";
 import type { RestClient } from "./rest";
 
+export type * from "./dynamo";
 export type * from "./event-bus";
 export type * from "./rest";
-export type * from "./dynamo";
 
 export type GatewayClient = DynamoClient | RestClient | EventBusClient;
 
