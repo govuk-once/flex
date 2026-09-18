a4a428cb2d40b3f27ab2965250aabd38fc4229e9
liamjones-png <liam.jones@digital.cabinet-office.gov.uk>
2026-09-18 08:07:08 +0100

    FLEX-503 chore: Enabled encrypted logs with rotated key (#524)
    * Enabled encrypted logs with rotated key
    
    * removed some comments
    
    * macie key dep

--- platform/infra/flex/src/stacks/macie.ts
+++ platform/infra/flex/src/stacks/macie.ts
@@ -9,6 +9,7 @@ import {
 import { Construct } from "constructs";
 
 import { BaseStack } from "../base";
+import { createLogGroupKey } from "../constructs/kms/log-group-key";
 import { MacieResultsBucket } from "../constructs/macie/MacieResultsBucket";
 import { macieCoverage } from "../macie-coverage";
 import { applyCheckovSkip } from "../utils/applyCheckovSkip";
@@ -31,6 +32,8 @@ export class FlexMacieStack extends BaseStack {
         region: "us-east-1",
       },
     });
+
+    createLogGroupKey(this, "alias/flex-macie-log-group-key");
 
     const sessionPhysicalId = PhysicalResourceId.of(
       `flex-macie-session-${this.account}-${this.region}`,
