cab4d3b21f6b5d8ba989896272feb7ecbd00f999
nathan berry <nathan.berry@digital.cabinet-office.gov.uk>
2026-09-21 12:08:42 +0100

    chore: add udp dashboard to cloudwatch

--- platform/infra/flex/src/stacks/cloudwatch-dashboards.ts
+++ platform/infra/flex/src/stacks/cloudwatch-dashboards.ts
@@ -31,9 +31,21 @@ export class FlexCloudWatchDashboardsStack extends BaseStack {
       ),
     );
 
+    const udpDashboard: unknown = JSON.parse(
+      fs.readFileSync(
+        path.join(dashboardsDir, "udpDashboard.json"),
+        "utf-8",
+      ),
+    );
+
     new CfnDashboard(this, "DrivingDashboard", {
       dashboardName: `${env}-flex-driving-dashboard`,
       dashboardBody: JSON.stringify(drivingDashboard),
+    });
+
+    new CfnDashboard(this, "DrivingDashboard", {
+      dashboardName: `${env}-flex-udp-dashboard`,
+      dashboardBody: JSON.stringify(udpDashboard),
     });
   }
 }
