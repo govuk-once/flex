bd889a609b94af85996d2cc6e80a42c961a714c7
nathan berry <nathan.berry@digital.cabinet-office.gov.uk>
2026-09-24 15:22:23 +0100

    chore: revert eslint config for testing package

--- libs/testing/eslint.config.mjs
+++ libs/testing/eslint.config.mjs
@@ -1,18 +1,3 @@
 import { config } from "@flex/config/eslint";
 
-export default [
-  ...config,
-  {
-    files: [
-      "src/fixtures/dynamo.ts",
-      "src/fixtures/dynamo.test.ts",
-      "src/fixtures/secret.ts",
-    ],
-    rules: {
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-    },
-  },
-];
+export default config;
